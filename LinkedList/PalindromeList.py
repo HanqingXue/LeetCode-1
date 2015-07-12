9bd3120f1769cd9c6107b3159a315d30252c637f
Eric <eche438@aucklanduni.ac.nz>
2015-07-12 19:38:47 +1200

    Stack-ish implementation of checking if a linked list is Palindrome

---
+++ LinkedList/PalindromeList.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @return {boolean}
+    def isPalindrome(self, head):
+        if head == None:
+            return True
+        current = head
+        value_list = []
+        while (current):
+            value_list.append(current.val)
+            current = current.next
+        value_list = value_list[::-1]
+        
+        idx = 0;
+        # reset current
+        current = head
+        while (current):
+            if current.val == value_list[idx]: 
+                current = current.next
+                idx += 1
+            else:
+                return False
+        return True
