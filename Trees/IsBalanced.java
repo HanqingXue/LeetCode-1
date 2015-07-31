611ce6518c5ba1b4dff895c42a119a89491c25a4
Eric <eche438@aucklanduni.ac.nz>
2015-08-01 11:44:46 +1200

    Modified comments

--- Trees/IsBalanced.java
+++ Trees/IsBalanced.java
@@ -12,19 +12,21 @@ For this problem, a height-balanced binary tree is defined as a binary tree in w
  */
 public class Solution {
     public boolean isBalanced(TreeNode root) {
+        /* Base Case */
         if (root == null) return true; // A tree is balanced if it has no nodes
         
-        int rHeight = height(root.right); // Find height of right side
-        int lHeight = height(root.left); // Find height of left side
+        int rHeight = height(root.right); // Find height of right subtree
+        int lHeight = height(root.left); // Find height of left subtree
         
-        // ABS minus is not greater than 1. And also every subtree is balanced, then return true;
+        // ABS minus is not greater than 1. And also recurse down every subtree check if balanced.
         if (Math.abs(rHeight - lHeight) <= 1 && isBalanced(root.right) && isBalanced(root.left)) return true;
-        else return false; // Otherwise one subtree isn't balanced then false
+        else return false; /* If we reach here then tree is not height-balanced */
     }
     
     
     /* Helper function; finds height of tree (max Depth) */
     public int height(TreeNode root) {
+        /* Base case */
         if (root == null) return 0; // No nodes means height is 0
         
         int lDepth = height(root.left); // Left side
